8a1b0f6705ae34ab3b3dd5a4504fa9a4b8588a20
Bati Sengul <batieatworld@gmail.com>
2009-01-11 23:16:14 +0000

    Major update on scan, added a searching utility. Try to rid the rest of the code from system calls

---
+++ search.cpp
@@ -0,0 +1,94 @@
+#include <cstring>
+#include <iostream>
+#include <dirent.h>
+#include <cstdlib>
+#include <sys/stat.h>
+#include <ftw.h>
+using namespace std;
+
+string tree;
+
+//Searches the directory for a file, if left empty returns whats in the directory with ,
+string search(string dir, string name=""){
+	int pos=0;
+	if(!strcmp(name.c_str(),""))
+		pos=-1;
+	else{ //Needed otherwise there is no substring!
+	//Find if there is a * so we can look gcc* would give true for gcc-4.3
+	if(!strcmp((name.substr(0,1)).c_str(),"*")){
+		name= name.substr(1);
+		pos=1;
+	}
+	if(!strcmp((name.substr(name.size()-1,1)).c_str(),"*")){
+		name= name.substr(0,name.size()-1);
+		pos=2;
+	}
+	}
+	//Begin the search
+	static DIR *dp=NULL;
+	struct dirent *d;
+	
+	dp=opendir(dir.c_str());
+	if(dp==NULL){
+		cerr<<"\nCannot search!"<<endl;
+		exit(1);
+	}
+	string ret;
+	ret="";
+	//string *temp=new string;
+	
+	//Loop for searching
+	if(pos>=0){
+	int findy=0;
+	while(d=readdir(dp)){
+		ret=d->d_name;
+		if(strcmp(ret.c_str(),".") && strcmp(ret.c_str(),"..")){
+		if((findy=ret.find(name))>=0){
+			if(findy>0 && pos==1){
+				closedir(dp);dp=NULL;
+				return ret;
+				}
+			if(findy==0 && pos==0 && ret.size()==name.size()){
+				closedir(dp);dp=NULL;
+				return ret;
+				}
+			if(findy==0 && pos==2){
+				closedir(dp);dp=NULL;
+				return ret;
+				}
+		}
+	}
+	}
+	}
+	
+	//Loop for listing
+	if(pos<0){
+		while(d=readdir(dp)){
+		ret=ret+d->d_name;
+		ret=ret+",";
+		}
+	closedir(dp);dp=NULL;
+	return ret.substr(0,ret.size()-1);
+	}
+	
+	closedir(dp);
+	dp=NULL;
+	return "";
+	
+
+}
+
+//We need this for the directory tree (I hate C strings)
+int list(const char* name, const struct stat *sp, int type){
+	if(type==FTW_D)
+		tree=tree+name+",";
+
+	return 0;
+
+}
+
+string dirTree(const string path){
+	tree="";
+	ftw(path.c_str(),list,12);
+	return tree.substr(0,tree.size()-1);
+}
---
+++ search.hpp
@@ -0,0 +1,7 @@
+#ifndef _SEARCH_H
+#define _SEARCH_H
+
+string search(string, string);
+string dirTree(const string);
+
+#endif
